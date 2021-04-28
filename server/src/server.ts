4333001cda87ad3f0e94c8c93d47930e3931b8be
Fernando Goncalves <fernando.hmg@gmail.com>
2021-04-28 00:38:32 -0300

    Controllers and new routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
+import cors from "cors";
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.post("/users", (request, response) => {
-  return response.json({ message: "Hello world!" });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server is running. http://localhost:3333");
